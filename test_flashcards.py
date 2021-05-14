222ed8b5fb73a83abe66f45782b55c6e53b2c33c
asadr98 <74267179+asadr98@users.noreply.github.com>
2021-05-14 15:27:30 -0400

    Add files via upload

---
+++ test_flashcards.py
@@ -0,0 +1,12 @@
+from flashcards import Flash
+from flashcards import Card
+import pytest
+
+def test_split_text_into_cards():
+    "Tests split_text_into_cards method of Flash class."
+    assert cards = [question, answer]
+
+def test_Card():
+    c = Card("alleviate", "to make better, easier")
+    assert c.question == "alleviate"
+    assert c.answer == "to make better, easier"
